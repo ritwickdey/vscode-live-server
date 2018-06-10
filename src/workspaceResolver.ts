a5b5050883a79d02429e3186f207cd33135a8bac
Ritwick Dey <ritwickdey@outlook.com>
2018-06-10 21:29:02 +0530

    workspaceResolver for mutiroot workspace

--- src/workspaceResolver.ts
+++ src/workspaceResolver.ts
@@ -1,8 +1,23 @@
-import { workspace } from 'vscode';
+import { workspace, window } from 'vscode';
 
 
 export const workspaceResolver = () => {
     return new Promise<string>(resolve => {
-        resolve(workspace.rootPath || '');
+        const { workspaceFolders } = workspace;
+        const workspaceNames = workspaceFolders.map(e => e.name);
+
+        // If only one workspace.
+        if (workspaceNames.length === 1) {
+            return resolve(workspaceFolders[0].uri.fsPath);
+        }
+
+        window.showQuickPick(workspaceNames, {
+            placeHolder: 'choose workspace for Live Server'
+        }).then(workspaceName => {
+            if (workspaceName) {
+                const workspaceUri = workspaceFolders.find(e => e.name === workspaceName).uri.fsPath;
+                resolve(workspaceUri);
+            }
+        });
     });
 };
