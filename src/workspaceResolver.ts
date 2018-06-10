8cc8d0f1697b8536cb3f9099859b6b06da7134a0
Ritwick Dey <ritwickdey@outlook.com>
2018-06-10 22:23:22 +0530

    more refactor

--- src/workspaceResolver.ts
+++ src/workspaceResolver.ts
@@ -7,31 +7,42 @@ export const workspaceResolver = () => {
         const { workspaceFolders } = workspace;
         const workspaceNames = workspaceFolders.map(e => e.name);
 
-        // If only one workspace.
+        // If only one workspace. No need to check anything.
         if (workspaceNames.length === 1) {
             return resolve(workspaceFolders[0].uri.fsPath);
         }
 
         // If workspace already set by User
         if (Config.getMutiRootWorkspaceName) {
-            // A small test that the WorkspaceName is valid
+            // A small test that the WorkspaceName (set by user) is valid
             const targetWorkspace = workspaceFolders.find(e => e.name === Config.getMutiRootWorkspaceName);
             if (targetWorkspace)
                 return resolve(targetWorkspace.uri.fsPath);
 
-            // reset whatever user is set;
+            // reset whatever user is set.
             Config.setMutiRootWorkspaceName(null);
         }
 
         // Show a quick picker
-        window.showQuickPick(workspaceNames, {
-            placeHolder: 'choose workspace for Live Server'
-        }).then(workspaceName => {
-            if (workspaceName) {
+        setOrChangeWorkspace()
+            .then(workspaceName => {
                 const workspaceUri = workspaceFolders.find(e => e.name === workspaceName).uri.fsPath;
-                Config.setMutiRootWorkspaceName(workspaceName)
-                    .then(() => resolve(workspaceUri));
-            }
-        });
+                return resolve(workspaceUri);
+            });
+    });
+};
+
+
+export const setOrChangeWorkspace = () => {
+    const { workspaceFolders } = workspace;
+    const workspaceNames = workspaceFolders.map(e => e.name);
+
+    return window.showQuickPick(workspaceNames, {
+        placeHolder: 'choose workspace for Live Server',
+        ignoreFocusOut: true
+    }).then(workspaceName => {
+        if (workspaceName) {
+            return Config.setMutiRootWorkspaceName(workspaceName).then(() => workspaceName);
+        }
     });
 };
