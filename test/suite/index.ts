4cb74aff0f5b2c86f69dd95f28c04a5ece45e0c6
Christian Bromann <git@bromann.dev>
2022-04-08 14:11:46 +0200

    fix type issues

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,13 +1,13 @@
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import Mocha from 'mocha';
+import glob from 'glob';
 
 export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
+    color: true
   });
-  mocha.useColors(true);
 
   const testsRoot = path.resolve(__dirname, '..');
 
